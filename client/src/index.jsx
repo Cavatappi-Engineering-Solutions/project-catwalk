6ab4f82f5ba731e03f5e23181c7d6fedd28dbacb
timapar <timothy_parrish@hotmail.com>
2021-06-04 14:37:32 -0400

    add redux boilerplate to create store and provide it to App

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
 
-const App = (props) => {
+import rootReducer from './stateManagement/reducers'
+
+const App = () => {
   return (
     <div>
-
+      <h1>Hi</h1>
     </div>
   )
 }
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
