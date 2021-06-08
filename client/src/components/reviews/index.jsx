438e872eccb1b2cf027279d2d41bc801ae8a1147
timapar <timothy_parrish@hotmail.com>
2021-06-08 10:15:46 -0400

    add header

--- client/src/components/reviews/index.jsx
+++ client/src/components/reviews/index.jsx
@@ -4,6 +4,7 @@ import Reviews from './reviews.jsx';
 
 const RatingsAndReviews = (props) => (
   <section id="ratings-reviews">
+    <h2>Ratings &amp; Reviews</h2>
     <Ratings />
     <Reviews />
   </section>
