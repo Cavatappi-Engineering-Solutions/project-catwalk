419138f52d86190ad05b6a18b53713a6675330b5
Andrew Huang <ahuang0225@gmail.com>
2021-06-08 17:42:10 -0400

    Used shorthand method instead

--- client/src/stateManagement/containers/product.js
+++ client/src/stateManagement/containers/product.js
@@ -3,19 +3,15 @@ import { retrieveProduct } from '../actions/product';
 import Product from '../../components/overview/Product.jsx';
 
 
-const mapStateToProps = state => {
-    return {
-        product: state.product
-    }
-}
+const mapStateToProps = state => ({
+    product: state.product
+})
 
-const mapDispatchToProps = dispatch => {
-    return {
-        retrieve: () => {
-            dispatch(retrieveProduct());
-        }
+const mapDispatchToProps = dispatch => ({
+    retrieve: () => {
+        dispatch(retrieveProduct());
     }
-}
+})
 
 const ProductContainer = connect(mapStateToProps, mapDispatchToProps)(Product);
 
--- client/src/stateManagement/containers/selectStyles.js
+++ client/src/stateManagement/containers/selectStyles.js
@@ -3,19 +3,15 @@ import { retrieveProductStyle } from '../actions/selectStyles';
 import ProductStyles from '../../components/overview/ProductStyles.jsx';
 
 
-const mapStateToProps = state => {
-    return {
-        productStyles: state.productStyles
-    }
-}
+const mapStateToProps = state => ({
+    productStyles: state.productStyles
+})
 
-const mapDispatchToProps = dispatch => {
-    return {
-        styles: () => {
-            dispatch(retrieveProductStyle());
-        }
+const mapDispatchToProps = dispatch => ({
+    styles: () => {
+        dispatch(retrieveProductStyle());
     }
-}
+})
 
 const ProductStylesContainer = connect(mapStateToProps, mapDispatchToProps)(ProductStyles);
 
