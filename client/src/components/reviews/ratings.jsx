d7fde33c3d66471b495aa66a9c3ecf26a595ddca
timapar <timothy_parrish@hotmail.com>
2021-06-08 12:55:05 -0400

    import all components and add basic structure

--- client/src/components/reviews/ratings.jsx
+++ client/src/components/reviews/ratings.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import NumeralRating from './numeralRating.jsx';
+import StarRating from './starRating.jsx';
+import PercentRecommend from './percentRecommend.jsx';
+import BarGraph from './barGraph.jsx';
+import Characteristics from './characteristics.jsx';
 
 const Ratings = (props) => (
   <section id="ratings">
-
+    {/* <NumeralRating />
+    <StarRating />
+    <PercentRecommend />
+    <BarGraph />
+    <Characteristics /> */}
   </section>
 )
 
