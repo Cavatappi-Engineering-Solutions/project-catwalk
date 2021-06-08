325fc1cc701a072a70ba2e452b43aa6835550772
timapar <timothy_parrish@hotmail.com>
2021-06-08 15:24:51 -0400

    fix to properly mapDispatch action creators

--- client/src/stateManagement/containers/reviews/ratingsAndReviewsContainer.js
+++ client/src/stateManagement/containers/reviews/ratingsAndReviewsContainer.js
@@ -1,9 +1,10 @@
 import {connect} from 'react-redux';
 import RatingsAndReviews from '../../../components/reviews/ratingsAndReviews.jsx';
-import reviewsReducer from '../../reducers/reviewsReducer.js'
+import {getProductReviews, updateProductReviews} from '../../actions/reviewsActions.js'
+import {getProductRatings, updateProductRatings} from '../../actions/ratingsActions.js'
 // Check that this reference is accurate once Andrew has product stored in state
 const mapState = (state) => ({product_id: 11001})
 
-const mapDispatch = {reviewsReducer}
+const mapDispatch = {getProductRatings, getProductReviews}
 
 export default connect(mapState, mapDispatch)(RatingsAndReviews);
