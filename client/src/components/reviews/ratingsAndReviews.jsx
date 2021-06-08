084aaf2c503aa1f13f56b6f8ea141f879583c26a
timapar <timothy_parrish@hotmail.com>
2021-06-08 15:12:01 -0400

    implement axios call for ratings data and dispatch to store

--- client/src/components/reviews/ratingsAndReviews.jsx
+++ client/src/components/reviews/ratingsAndReviews.jsx
@@ -5,16 +5,29 @@ import store from '../../index.jsx'
 import Ratings from './ratings.jsx';
 import Reviews from './reviews.jsx';
 import {getProductReviews} from '../../stateManagement/actions/reviewsActions.js';
+import {getProductRatings} from '../../stateManagement/actions/ratingsActions.js';
 
 const RatingsAndReviews = ({product_id, reviewsReducer}) => {
-  const config = {
+  const reviewConfig = {
     method: 'GET',
     url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews/',
     headers: {Authorization: API_KEY},
     params: {product_id}
   }
-  axios(config)
-    .then(({data}) => store.dispatch(getProductReviews(data.results)))
+  const ratingConfig = {
+    method: 'GET',
+    url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews/meta/',
+    headers: {Authorization: API_KEY},
+    params: {product_id}
+  }
+  axios.all([axios(reviewConfig), axios(ratingConfig)])
+    .then(axios.spread((...res) => {
+      const reviews = res[0].data.results;
+      const ratings = res[1].data.ratings;
+
+      store.dispatch(getProductReviews(reviews));
+      store.dispatch(getProductRatings(ratings));
+    }))
     .catch((err) => console.log(err))
 
   return (
