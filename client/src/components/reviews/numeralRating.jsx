fee183d5c0828fbdcb60eec7474fdeb7b4024403
timapar <timothy_parrish@hotmail.com>
2021-06-10 19:15:39 -0400

    fix linter issues and remove console log

--- client/src/components/reviews/numeralRating.jsx
+++ client/src/components/reviews/numeralRating.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-const NumeralRating = ({ratings}) => {
-  let total = 0;
-  let sum = 0;
-  for (let key in ratings) {
-    sum += key * ratings[key];
-    total += Number(ratings[key]);
+const NumeralRating = ({ ratings }) => {
+  let total = 0
+  let sum = 0
+  for (const key in ratings) {
+    sum += key * ratings[key]
+    total += Number(ratings[key])
   }
-  console.log('total: ', total, 'sum: ', sum)
-  const weightedAvg = sum / total;
-  const roundedRating = Math.round((weightedAvg + Number.EPSILON) * 10) / 10;
+  const weightedAvg = sum / total
+  const roundedRating = Math.round((weightedAvg + Number.EPSILON) * 10) / 10
 
   return <span id="numeral-rating">{roundedRating ? roundedRating.toString() : ''}</span>
 }
 
-export default NumeralRating;
+export default NumeralRating
