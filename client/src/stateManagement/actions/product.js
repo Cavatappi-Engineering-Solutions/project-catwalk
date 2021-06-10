c894655ba12a41b8b8ac409832cb9405d8817c02
Andrew Huang <ahuang0225@gmail.com>
2021-06-10 13:14:39 -0400

    Renamed function name

--- client/src/stateManagement/actions/product.js
+++ client/src/stateManagement/actions/product.js
@@ -1,28 +1,28 @@
-import axios from 'axios';
+import axios from 'axios'
 import { API_KEY } from '../../../../config.js'
 
 export const type = {
-    GET_PRODUCT: 'GET_PRODUCT'
-};
+  GET_PRODUCT: 'GET_PRODUCT'
+}
 
 const getProducts = (data) => ({
-    type: type.GET_PRODUCT,
-    payload: data
+  type: type.GET_PRODUCT,
+  payload: data
 })
 
 export const retrieveProduct = () => {
-    return (dispatch) => {
-        const options = {
-            method: 'GET',
-            url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/11001',
-            headers: { 'Authorization': API_KEY }
-        };
-        return axios(options)
-            .then(({data}) => { 
-                dispatch(getProducts(data));
-            })
-            .catch((error) => { 
-                console.log(error);
-            })
+  return (dispatch) => {
+    const options = {
+      method: 'GET',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/11001',
+      headers: { Authorization: API_KEY }
     }
+    return axios(options)
+      .then(({ data }) => {
+        dispatch(getProducts(data))
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 }
