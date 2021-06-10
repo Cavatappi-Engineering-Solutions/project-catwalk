e6dcb2cb9793a62d6668d2b52273ab2763f33d19
timapar <timothy_parrish@hotmail.com>
2021-06-10 10:13:59 -0400

    add recommendReducer and characterReducer

--- client/src/stateManagement/reducers/index.js
+++ client/src/stateManagement/reducers/index.js
@@ -1,12 +1,16 @@
-import { combineReducers } from 'redux';
-import productReducer from './product';
-import productStylesReducer from './selectStyles';
+import { combineReducers } from 'redux'
+import productReducer from './product'
+import productStylesReducer from './selectStyles'
 import reviewsReducer from './reviewsReducer.js'
 import ratingsReducer from './ratingsReducer.js'
+import recommendReducer from './recommendReducer.js'
+import characterReducer from './characterReducer.js'
 
 export default combineReducers({
   product: productReducer,
   productStyles: productStylesReducer,
   reviews: reviewsReducer,
-  ratings: ratingsReducer
-});
+  ratings: ratingsReducer,
+  recommeded: recommendReducer,
+  characteristics: characterReducer
+})
