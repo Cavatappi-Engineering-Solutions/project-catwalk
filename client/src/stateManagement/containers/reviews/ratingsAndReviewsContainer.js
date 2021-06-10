c6d178006cb4f64d37ab2a3475654717a89b5690
timapar <timothy_parrish@hotmail.com>
2021-06-10 09:48:21 -0400

    add Recommended and Characteristics actions to dispatch

--- client/src/stateManagement/containers/reviews/ratingsAndReviewsContainer.js
+++ client/src/stateManagement/containers/reviews/ratingsAndReviewsContainer.js
@@ -1,10 +1,15 @@
-import {connect} from 'react-redux';
-import RatingsAndReviews from '../../../components/reviews/ratingsAndReviews.jsx';
-import {getProductReviews, updateProductReviews} from '../../actions/reviewsActions.js'
-import {getProductRatings, updateProductRatings} from '../../actions/ratingsActions.js'
+import { connect } from 'react-redux'
+import RatingsAndReviews from '../../../components/reviews/ratingsAndReviews.jsx'
+import { getProductReviews, updateProductReviews } from '../../actions/reviewsActions.js'
+import { getProductRatings, getProductRecommended, getProductCharacteristics } from '../../actions/ratingsActions.js'
 // Check that this reference is accurate once Andrew has product stored in state
-const mapState = (state) => ({product_id: 11001})
+const mapState = (state) => ({ product_id: 11001 })
 
-const mapDispatch = {getProductRatings, getProductReviews}
+const mapDispatch = {
+  getProductRatings,
+  getProductReviews,
+  getProductRecommended,
+  getProductCharacteristics
+}
 
-export default connect(mapState, mapDispatch)(RatingsAndReviews);
+export default connect(mapState, mapDispatch)(RatingsAndReviews)
