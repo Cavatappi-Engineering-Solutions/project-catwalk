aac7f9c9bf633626754c0f368ce2496b605c703b
timapar <timothy_parrish@hotmail.com>
2021-06-11 12:22:06 -0400

    create component with name and bar

---
+++ client/src/components/reviews/characteristic.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Characteristic = ({ name, value }) => (
+  <div className="characteristic">
+    <span className="char-name">{name}</span>
+      <div>
+        <div className="char-bar"></div>
+        <span className="char-marker" style={{ '--marker-position': `${(value / 5 * 100) - 3}%`}}>&#9660;</span>
+      </div>
+    <div className="char-meaning">
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+)
+
+export default Characteristic
