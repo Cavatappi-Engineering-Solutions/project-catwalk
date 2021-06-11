e35d98387746d7f91e585cc22b5bef8e4423d822
Andrew Huang <ahuang0225@gmail.com>
2021-06-11 14:12:23 -0400

    Deleted/Reloacted SKU component into AddToCart component

--- client/src/components/overview/AddToCart.jsx
+++ client/src/components/overview/AddToCart.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
-const AddToCart = () => (
-  <div>
-    <h4>Placeholder</h4>
-  </div>
-)
+const AddToCart = ({ productStyles, currentSelectedStyle, changeSKU, quantity }) => {
+  const quantityArray = (quantity) => {
+    let array = []
+    if (!quantity) { array = ['_'] }
+    if (quantity > 15) { quantity = 15 }
+    for (let i = 1; i <= quantity; i++) { array.push(i) }
+    return array
+  }
+  return (
+    !productStyles.results
+      ? <div className='carousel'>PLEASE WAIT</div>
+      : <div className='carousel'>
+        <select onChange={(e) => { changeSKU(e.target.value) }}>
+          {Object.values(productStyles.results[currentSelectedStyle].skus).map(({ quantity, size }, index) => (
+            <option value={quantity} key={index}>
+              {size}
+            </option>
+          ))}
+        </select>
+        <select>
+          {quantityArray(quantity).map((inventory, index) =>
+          <option key={index}>
+            {inventory}
+          </option>)}
+        </select>
+        <button>Add To Cart</button>
+      </div>
+  )
+}
 
 export default AddToCart
