e2b076a638b1a74e9c1ff2abea909ee07cfccea0
timapar <timothy_parrish@hotmail.com>
2021-06-11 18:03:31 -0400

    implement the meanings below the bar

--- client/src/components/reviews/characteristic.jsx
+++ client/src/components/reviews/characteristic.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const Characteristic = ({ name, value }) => (
+const Characteristic = ({ name, characteristics }) => (
   <div className="characteristic">
     <span className="char-name">{name}</span>
       <div>
         <div className="char-bar"></div>
-        <span className="char-marker" style={{ '--marker-position': `${(value / 5 * 100) - 3}%` }}>&#9660;</span>
+        <div className="char-marker" style={{ '--marker-position': `${(characteristics.value / 5 * 100) - 3}%` }}>&#9660;</div>
       </div>
     <div className="char-meaning">
-      <span></span>
-      <span></span>
+      <span className="char-meaning-low">{characteristics.low}</span>
+      <span className="char-meaning-high">{characteristics.high}</span>
     </div>
   </div>
 )
