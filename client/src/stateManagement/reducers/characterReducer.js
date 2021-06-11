f249f75cff34641603cf2fa8c3221613ca836121
timapar <timothy_parrish@hotmail.com>
2021-06-11 16:45:31 -0400

    implement addition of meanings to each characteristic

--- client/src/stateManagement/reducers/characterReducer.js
+++ client/src/stateManagement/reducers/characterReducer.js
@@ -1,7 +1,20 @@
 const characterReducer = (characteristics = {}, action) => {
   switch (action.type) {
-    case 'GET_CHARACTER':
-      return action.payload
+    case 'GET_CHARACTER': {
+      const meanings = {
+        Fit: { low: 'Poor', high: 'Perfect' },
+        Size: { low: 'Too small', high: 'Too large' },
+        Width: { low: 'Too narrrow', high: 'Too wide' },
+        Length: { low: 'Too short', high: 'Too long' },
+        Comfort: { low: 'Poor', high: 'Perfect' },
+        Quality: { low: 'Poor', high: 'High' }
+      }
+      const charAndMeanings = {}
+      for (const key in action.payload) {
+        charAndMeanings[key] = Object.assign(action.payload[key], meanings[key])
+      }
+      return charAndMeanings
+    }
     default:
       return characteristics
   }
