59f2fa23df2058e12c85fa8166cdf35b9c7de6ca
Andrew Huang <ahuang0225@gmail.com>
2021-06-11 14:15:58 -0400

    Removed product default price

--- client/src/components/overview/Product.jsx
+++ client/src/components/overview/Product.jsx
@@ -5,7 +5,6 @@ const Product = ({ product }) => (
     <section>
       <h2 className='product_name'>{product.name}</h2>
       <h2 className='product_name'>{product.category}</h2>
-      <p className='product_name'>{product.default_price}</p>
       <h3 className='product_name'>{product.slogan}</h3>
       <p className='product_name'>{product.description}</p>
     </section>
