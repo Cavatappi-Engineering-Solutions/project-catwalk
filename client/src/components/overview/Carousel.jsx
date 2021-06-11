38283fe821a790193d2f860abf19b9204a9895fc
Andrew Huang <ahuang0225@gmail.com>
2021-06-11 14:14:16 -0400

    Refactored Carousel Component

--- client/src/components/overview/Carousel.jsx
+++ client/src/components/overview/Carousel.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
 
-const Carousel = ({ productStyles, previousStyle, nextStyle, length, currentSelectedStyle }) => (
+const Carousel = ({ productStyles, previousStyle, nextStyle, length, currentSelectedStyle, changeView }) => (
   !productStyles.results
     ? <div className='carousel'>PLEASE WAIT</div>
     : <div className='carousel'>
+        <button onClick={() => { changeView() }}>O</button>
         <section className='product_urls'>
           <FaArrowAltCircleLeft className='left_arrow' onClick={() => { previousStyle() }}/>
           <FaArrowAltCircleRight className='right_arrow' onClick={() => { nextStyle() }}/>
