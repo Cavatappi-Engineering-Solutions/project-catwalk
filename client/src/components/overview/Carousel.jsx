5630cfd398cba9dd8d456d817d3203cb7ad549dc
Andrew Huang <ahuang0225@gmail.com>
2021-06-12 13:36:59 -0400

    Refactor map to reduce

--- client/src/components/overview/Carousel.jsx
+++ client/src/components/overview/Carousel.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
 
-const Carousel = ({ productStyles, previousStyle, nextStyle, length, currentSelectedStyle, changeView }) => (
-  !productStyles.results
-    ? <div className='carousel'>PLEASE WAIT</div>
-    : <div className='carousel'>
-        <button onClick={() => { changeView() }}>O</button>
-        <section className='product_urls'>
-          <FaArrowAltCircleLeft className='left_arrow' onClick={() => { previousStyle() }}/>
-          <FaArrowAltCircleRight className='right_arrow' onClick={() => { nextStyle() }}/>
-          {productStyles.results[currentSelectedStyle].photos.map((photo, index) => (
-            <div key={index}>
-              {index === length && (<img src={`${photo.url}`} className='product_url'/>)}
-            </div>
-          )
-          )}
-        </section>
-    </div>
-)
+const Carousel = ({ currentStylePhotos, productStyles, previousStyle, nextStyle, length, currentSelectedStyle, changeView }) => {
+  const matchingProductStyle = currentStylePhotos
+    ? currentStylePhotos.reduce(
+      (acc, photo, index) => {
+        if (index === length) acc = photo
+        return acc
+      },
+      null
+    )
+    : {}
+  return (
+    !currentStylePhotos
+      ? <div className='carousel'>PLEASE WAIT</div>
+      : <div className='carousel'>
+          <button onClick={() => { changeView() }}>O</button>
+          <section className='product_urls'>
+            <FaArrowAltCircleLeft className='left_arrow' onClick={() => { previousStyle() }}/>
+            <FaArrowAltCircleRight className='right_arrow' onClick={() => { nextStyle() }}/>
+            {matchingProductStyle
+              ? <div>
+                  <img src={`${matchingProductStyle.url}`} className="current_display_photo"/>
+                </div>
+              : <span>Unable to Load Image</span>}
+          </section>
+      </div>
+  )
+}
 
 export default Carousel
