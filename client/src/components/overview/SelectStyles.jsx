b41c405202e39d75b6cf9d57ff77be0d98e943d0
Andrew Huang <ahuang0225@gmail.com>
2021-06-12 13:35:28 -0400

    Passed down a more approiate state

--- client/src/components/overview/SelectStyles.jsx
+++ client/src/components/overview/SelectStyles.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
-const ProductStyles = ({ productStyles, currentSelectedStyle, changeSelectedStyle }) => (
-  !productStyles.results
+const ProductStyles = ({ productStyles, currentSelectedStyle, changeSelectedStyle, currentStyle }) => (
+  !currentStyle
     ? <div className='product_styles'>PLEASE WAIT</div>
     : <div className='product_styles'>
-        <div className='style_name'>{productStyles.results[currentSelectedStyle].name}</div>
-        <div className='style_price'>{productStyles.results[currentSelectedStyle].sale_price
-          ? <div className='sale_price'>{`${productStyles.results[currentSelectedStyle].sale_price}/ ${productStyles.results[currentSelectedStyle].original_price}`}</div>
-          : <div className='default_price'>{productStyles.results[currentSelectedStyle].original_price}</div>}
+        <div className='style_name'>{currentStyle.name}</div>
+        <div className='style_price'>{currentStyle.sale_price
+          ? <div className='sale_price'>{`${currentStyle.sale_price}/ ${currentStyle.original_price}`}</div>
+          : <div className='default_price'>{currentStyle.original_price}</div>}
       </div>
         {productStyles.results.map((style, index) => (
             <div className='select_styles' key={index}>
               {currentSelectedStyle !== index &&
               (<img
               src={`${style.photos[0].thumbnail_url}`}
-              className='product_url'
+              className='select_styles'
               onClick={() => { changeSelectedStyle(index) }}/>
               )}
             </div>
