d8bcc5c3eda26994bff949ac50f1e90718fd9549
timapar <timothy_parrish@hotmail.com>
2021-06-12 14:59:52 -0400

    update to reflect new reviews state format

--- client/src/stateManagement/containers/reviews/reviewDisplayContainer.js
+++ client/src/stateManagement/containers/reviews/reviewDisplayContainer.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux'
 import ReviewDisplay from '../../../components/reviews/reviewDisplay.jsx'
 
-const mapStateToProps = (state) => ({ reviews: state.reviews })
+const mapStateToProps = (state) => ({ reviews: state.reviews.reviews })
 
 export default connect(mapStateToProps)(ReviewDisplay)
