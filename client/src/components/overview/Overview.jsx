16b5d9a904b9fa5bcd7829ca848e2a0dc9313226
Andrew Huang <ahuang0225@gmail.com>
2021-06-12 13:38:44 -0400

    Created alias for state

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -4,7 +4,7 @@ import SelectStyles from './SelectStyles.jsx'
 import Carousel from './Carousel.jsx'
 import AddToCart from './AddToCart.jsx'
 import DefaultView from './DefaultView.jsx'
-import NumeralRating from '../reviews/numeralRating.jsx'
+import ProductDescription from './ProductDescription.jsx'
 import StarRating from '../reviews/starRating.jsx'
 
 class Overview extends React.Component {
@@ -13,7 +13,6 @@ class Overview extends React.Component {
     this.state = {
       view: false,
       currentProduct: '',
-      currentPhoto: 0,
       currentSelectedStyle: 0,
       length: 0,
       quantity: ''
@@ -35,22 +34,23 @@ class Overview extends React.Component {
   }
 
   imageGallery () {
-    const { view, length, currentSelectedStyle, currentPhoto } = this.state
+    const { view, length, currentSelectedStyle } = this.state
     const { productStyles } = this.props
     if (view) {
       return <Carousel
       productStyles={ productStyles }
+      currentSelectedStyle= { currentSelectedStyle }
+      currentStylePhotos={ productStyles.results?.[currentSelectedStyle].photos }
       previousStyle={ this.previousStyle }
       nextStyle={ this.nextStyle }
       length={ length }
-      currentSelectedStyle= { currentSelectedStyle }
       changeView={ this.changeView }
       />
     } else {
       return <DefaultView
       productStyles={ productStyles }
       currentSelectedStyle={ currentSelectedStyle }
-      currentPhoto={ currentPhoto }
+      currentStylePhotos={ productStyles.results?.[currentSelectedStyle].photos }
       changeView={ this.changeView }
       changeMainPhoto={ this.changeMainPhoto }
       length={ length }
@@ -61,7 +61,7 @@ class Overview extends React.Component {
   }
 
   changeMainPhoto (index) {
-    this.setState({ currentPhoto: index, length: index })
+    this.setState({ length: index })
   }
 
   changeSKU (quantity) {
@@ -100,35 +100,43 @@ class Overview extends React.Component {
     const { currentSelectedStyle, quantity } = this.state
     const { product, productStyles, ratings } = this.props
     return (
-      <div>
-        <section id='view'>
-          { this.imageGallery() }
+      <section id='overview'>
+        <section id='combined_view_product_information'>
+          <section id='view'>
+            { this.imageGallery() }
+          </section>
+          <section id='product_information'>
+            <section id='ratings' className='container'>
+              <div className='container-row'>
+              <StarRating ratings={ ratings }/>
+              </div>
+            </section>
+            <section>
+              <Product product={ product }/>
+            </section>
+            <section id='product_styles'>
+              <SelectStyles
+              productStyles={ productStyles }
+              currentSelectedStyle= { currentSelectedStyle }
+              changeSelectedStyle={ this.changeSelectedStyle }
+              currentStyle={ productStyles.results?.[currentSelectedStyle] }
+              />
+            </section>
+            <section id='add_to_cart'>
+              <AddToCart
+              quantity={ quantity }
+              productStyles={ productStyles }
+              currentSelectedStyle= { currentSelectedStyle }
+              changeSKU={ this.changeSKU }
+              currentStyle={ productStyles.results?.[currentSelectedStyle] }
+              />
+            </section>
+          </section>
         </section>
-        <section id='ratings' className='container'>
-          <div className='container-row'>
-          <NumeralRating ratings={ ratings }/>
-          <StarRating ratings={ ratings }/>
-          </div>
+        <section id='product_description'>
+          <ProductDescription product={ product }/>
         </section>
-        <section id='product_information'>
-          <Product product={ product }/>
-        </section>
-        <section id='product_styles'>
-          <SelectStyles
-          productStyles={ productStyles }
-          currentSelectedStyle= { currentSelectedStyle }
-          changeSelectedStyle={ this.changeSelectedStyle }
-          />
-        </section>
-        <section id='add_to_cart'>
-          <AddToCart
-          quantity={ quantity }
-          productStyles={ productStyles }
-          currentSelectedStyle= { currentSelectedStyle }
-          changeSKU={ this.changeSKU }
-          />
-        </section>
-      </div>
+      </section>
     )
   }
 }
