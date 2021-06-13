221ae5db558f457f1d2aad3ae6fdd258e7790c8b
Andrew Huang <ahuang0225@gmail.com>
2021-06-12 21:15:20 -0400

    Relocated Rating to Product Component

--- client/src/components/overview/Product.jsx
+++ client/src/components/overview/Product.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import NumeralRating from './CopyOfNumeralRating.jsx'
+import StarRating from '../reviews/starRating.jsx'
 
-const Product = ({ product }) => (
-  <div className='product_info'>
+const Product = ({ product, ratings }) => (
+  <div>
     <section>
-      <h2 className='product_name'>{product.category}</h2>
       <h2 className='product_name'>{product.name}</h2>
+      <section id='ratings' className='container'>
+        <div className='container-row'>
+        <NumeralRating ratings={ ratings }/>
+        <StarRating ratings={ ratings }/>
+        </div>
+      </section>
+      <h4 className='product_name'>{product.category}</h4>
     </section>
   </div>
 )
