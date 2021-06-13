0175a30ba837b142b7dba2c9df27db06f2b77f49
Andrew Huang <ahuang0225@gmail.com>
2021-06-13 16:28:51 -0400

    Added className

--- client/src/components/overview/Carousel.jsx
+++ client/src/components/overview/Carousel.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
+import Enlarge from '../../assets/icons/enlarge.png'
 
-const Carousel = ({ currentStylePhotos, productStyles, previousStyle, nextStyle, length, currentSelectedStyle, changeView }) => {
+const Carousel = ({ currentStylePhotos, previousStyle, nextStyle, length, changeView }) => {
   const matchingProductStyle = currentStylePhotos
     ? currentStylePhotos.reduce(
       (acc, photo, index) => {
@@ -16,13 +17,13 @@ const Carousel = ({ currentStylePhotos, productStyles, previousStyle, nextStyle,
       ? <div className='carousel'>PLEASE WAIT</div>
       : <div className='carousel'>
           <section className='expand_view_button_wrap'>
-            <button onClick={() => { changeView() }} className='expand_view_button'>O</button>
+            <button onClick={() => { changeView() }} className='expand_view_button'><img src={Enlarge}/></button>
           </section>
           <section className='left_right_arrow_wrap'>
             <FaArrowAltCircleLeft className='left_arrow' onClick={() => { previousStyle() }}/>
             <FaArrowAltCircleRight className='right_arrow' onClick={() => { nextStyle() }}/>
           </section>
-          <section className='product_urls'>
+          <section className='carousel_display'>
             {matchingProductStyle
               ? <div>
                   <img src={`${matchingProductStyle.url}`} className="carousel_current_display_photo"/>
