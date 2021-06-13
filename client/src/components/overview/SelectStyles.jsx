6d8227c1c33d06dc5bfca5bb62f2cc191cf19ffe
Andrew Huang <ahuang0225@gmail.com>
2021-06-13 16:26:55 -0400

    Changed name

--- client/src/components/overview/SelectStyles.jsx
+++ client/src/components/overview/SelectStyles.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ProductStyles = ({ productStyles, currentSelectedStyle, changeSelectedStyle, currentStyle }) => (
+const ProductStyles = ({ productStyles, changeSelectedStyle, currentStyle }) => (
   !currentStyle
     ? <div className='product_styles'>PLEASE WAIT</div>
     : <div className='product_styles'>
         <div className='style_price'>{currentStyle.sale_price
           ? <div className='sale_price'>{`Price: $${currentStyle.sale_price}/ $${currentStyle.original_price}`}</div>
           : <div className='default_price'>{`Price: $${currentStyle.original_price}`}</div>}
-          <div className='style_name'>{`Style > ${currentStyle.name}`}</div>
+          <div className='style_name'>{`Style: ${currentStyle.name}`}</div>
         </div>
         <section className='styles_container'>
           {productStyles.results.map((style, index) => (
