857205d44d590db0de77d47bac2e2a6fa1e95cb7
Andrew Huang <ahuang0225@gmail.com>
2021-06-13 22:33:21 -0400

    Removed unnesscary section element

--- client/src/components/overview/ProductDescription.jsx
+++ client/src/components/overview/ProductDescription.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 
 const ProductDescription = ({ product }) => (
   <div className='product_slogan_description'>
-    <section>
-      <h3 className='product_slogan'>{product.slogan}</h3>
-      <p className='product_description'>{product.description}</p>
-    </section>
+    <h3 className='product_slogan'>{product.slogan}</h3>
+    <p className='product_description'>{product.description}</p>
   </div>
 )
 
