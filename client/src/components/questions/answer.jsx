cc50dfc8a6d7a1b7a7a54e8bd960933de544fe7a
MarcAnthony Petrecca <marcanthony.petrecca@gmail.com>
2021-06-14 19:14:12 -0400

    Fix unique key error

--- client/src/components/questions/answer.jsx
+++ client/src/components/questions/answer.jsx
@@ -12,7 +12,7 @@ const Answer = (props) => {
         storage.push(answers[key])
     }
 
-    const list = <div>{storage.map((answer) => <div>{answer.answerer_name}&nbsp;{answer.body}&nbsp;{answer.date}&nbsp;Helpful? Yes({helpfulTag(answer.helpfulness)})</div>)}</div>
+    const list = <div>{storage.map((answer) => <div key={answer.id}>{answer.answerer_name}&nbsp;{answer.body}&nbsp;{answer.date}&nbsp;Helpful? Yes({helpfulTag(answer.helpfulness)})</div>)}</div>
     const empty = <div>There are no answers!</div>
 
     return (
