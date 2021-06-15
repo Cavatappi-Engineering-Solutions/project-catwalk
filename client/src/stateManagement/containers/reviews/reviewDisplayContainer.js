b257ae217670b993d3b74ae1f72895317b3f2118
timapar <timothy_parrish@hotmail.com>
2021-06-15 14:35:54 -0400

    map count to component as props

--- client/src/stateManagement/containers/reviews/reviewDisplayContainer.js
+++ client/src/stateManagement/containers/reviews/reviewDisplayContainer.js
@@ -1,6 +1,9 @@
 import { connect } from 'react-redux'
 import ReviewDisplay from '../../../components/reviews/reviewDisplay.jsx'
 
-const mapStateToProps = (state) => ({ reviews: state.reviews.reviews })
+const mapStateToProps = (state) => ({
+  reviews: state.reviews.reviews,
+  count: state.reviews.count
+})
 
 export default connect(mapStateToProps)(ReviewDisplay)
