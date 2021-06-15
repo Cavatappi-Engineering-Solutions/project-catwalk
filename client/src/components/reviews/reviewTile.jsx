8e614ff1a05a416806238f711ae76db975411017
timapar <timothy_parrish@hotmail.com>
2021-06-15 15:28:54 -0400

    change many span elements to p

--- client/src/components/reviews/reviewTile.jsx
+++ client/src/components/reviews/reviewTile.jsx
@@ -5,11 +5,11 @@ const Review = ({ data }) => (
   <div className="review-tile container-col">
     <div className="tile-header">
       <StarRating ratings={{ [data.rating]: '1' }} total={1} />
-      <span>{`${data.reviewer_name}, ${data.date}`}</span>
+      <span className="tile-date">{`${data.reviewer_name}, ${data.date}`}</span>
     </div>
-    <span className="tile-summary">{data.summary}</span>
+    <p className="tile-summary">{data.summary}</p>
     <p className="tile-body">{data.body}</p>
-    <span className="tile-recommend">{data.recommend}</span>
+    <p className="tile-recommend">{data.recommend}</p>
     <p className="tile-response">{data.response}</p>
     <span className="tile-helpful"></span>
   </div>
