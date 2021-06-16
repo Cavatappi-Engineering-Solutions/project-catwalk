8fe88dc49b0be98ce43e6f81dbcd0d69d4516b25
timapar <timothy_parrish@hotmail.com>
2021-06-16 08:42:18 -0400

    add dispatch in .then

--- client/src/stateManagement/actions/reviewsActions.js
+++ client/src/stateManagement/actions/reviewsActions.js
@@ -11,15 +11,20 @@ const changeSort = (sortParam) => ({
   payload: sortParam
 })
 
+const incrementHelpful = (review_id) => ({
+  type: 'INCREMENT_HELPFUL',
+  payload: review_id
+})
+
 const markReviewHelpful = (review_id) => {
-  return () => {
+  return (dispatch) => {
     const helpfulConfig = {
       method: 'PUT',
       url: `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews/${review_id}/helpful`,
       headers: { Authorization: API_KEY }
     }
     axios(helpfulConfig)
-      .then()
+      .then(() => dispatch(incrementHelpful(review_id)))
       .catch((err) => console.log('There was an error marking this review as helpful: ', err))
   }
 }
