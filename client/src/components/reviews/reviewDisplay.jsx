4db95dbb5cb5648ff955d2801bf771feaefd6cff
timapar <timothy_parrish@hotmail.com>
2021-06-15 20:34:15 -0400

    pass markHelpful down as props

--- client/src/components/reviews/reviewDisplay.jsx
+++ client/src/components/reviews/reviewDisplay.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Review from './reviewTile.jsx'
 
-const ReviewDisplay = ({ reviews, count }) => {
+const ReviewDisplay = ({ reviews, count, markReviewHelpful }) => {
   if (reviews.length < 1) {
     return <div>There are no reviews that match</div>
   } else {
     const reviewList = []
     for (let i = 0; i < count; i++) {
-      reviewList.push((<Review data={reviews[i]} key={reviews[i].review_id} />))
+      reviewList.push((<Review
+        data={reviews[i]}
+        key={reviews[i].review_id}
+        markHelpful={markReviewHelpful}
+      />))
     }
 
     return (
