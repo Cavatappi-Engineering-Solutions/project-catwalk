7dabc9a97a4f02806cf03a27d716d79d3a9adfaa
timapar <timothy_parrish@hotmail.com>
2021-06-15 20:35:38 -0400

    map markReviewHelpful to dispatch for component

--- client/src/stateManagement/containers/reviews/reviewDisplayContainer.js
+++ client/src/stateManagement/containers/reviews/reviewDisplayContainer.js
@@ -1,9 +1,12 @@
 import { connect } from 'react-redux'
 import ReviewDisplay from '../../../components/reviews/reviewDisplay.jsx'
+import { markReviewHelpful } from '../../actions/reviewsActions.js'
 
 const mapStateToProps = (state) => ({
   reviews: state.reviews.reviews,
   count: state.reviews.count
 })
 
-export default connect(mapStateToProps)(ReviewDisplay)
+const mapDispatchToProps = { markReviewHelpful }
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewDisplay)
