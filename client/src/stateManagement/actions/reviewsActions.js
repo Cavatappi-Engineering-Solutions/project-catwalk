534f0941cb877d9f99d8a82ce3aa08881eb4fe02
timapar <timothy_parrish@hotmail.com>
2021-06-15 20:33:12 -0400

    implement the PUT request to mark a review as helpful
    STILL NEED TO FIGURE OUT HOW TO THEN MAKE THE RERENDER

--- client/src/stateManagement/actions/reviewsActions.js
+++ client/src/stateManagement/actions/reviewsActions.js
@@ -1,10 +1,8 @@
+import axios from 'axios'
+import { API_KEY } from '../../../../config.js'
+
 const getProductReviews = (productReviews) => ({
   type: 'GET_REVIEWS',
-  payload: productReviews
-})
-
-const updateProductReviews = (productReviews) => ({
-  type: 'UPDATE_REVIEWS',
   payload: productReviews
 })
 
@@ -13,4 +11,17 @@ const changeSort = (sortParam) => ({
   payload: sortParam
 })
 
-export { getProductReviews, updateProductReviews, changeSort }
+const markReviewHelpful = (review_id) => {
+  return () => {
+    const helpfulConfig = {
+      method: 'PUT',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews/${review_id}/helpful`,
+      headers: { Authorization: API_KEY }
+    }
+    axios(helpfulConfig)
+      .then()
+      .catch((err) => console.log('There was an error marking this review as helpful: ', err))
+  }
+}
+
+export { getProductReviews, markReviewHelpful, changeSort }
