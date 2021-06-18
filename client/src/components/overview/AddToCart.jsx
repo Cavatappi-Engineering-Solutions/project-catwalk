ef04a4da100a62b2d3f61f0add07b19786e8c3fd
Andrew Huang <ahuang0225@gmail.com>
2021-06-18 11:27:48 -0400

    Updated AddToCart and CSS

--- client/src/components/overview/AddToCart.jsx
+++ client/src/components/overview/AddToCart.jsx
@@ -14,29 +14,33 @@ const AddToCart = ({ changeSKU, quantity, currentStyle, icon, changeIcon, addToC
     !currentStyle
       ? <section className='selection'>PLEASE WAIT</section>
       : <section className='selection'>
-        <select onChange={(e) => { changeSKU(e.target.value) }} className='size_dropdown'>
-          <option hidden>SELECT SIZE</option>
-          {Object.values(currentStyle.skus).map(({ quantity, size }, index) => (
-            <option value={quantity} key={index}>
-              {size}
-            </option>
-          ))}
-        </select>
-        <select className='quantity_dropdown'>
-          <option hidden>_</option>
-          {quantityArray(quantity).map((inventory, index) =>
-            (!inventory
-              ? <option>OUT OF STOCK</option>
-              : <option key={index}>{inventory}</option>))}
-        </select>
-        <button className='cart' aria-label="add to cart" onClick={() => addToCartClick()}>Add To Cart</button>
-        {icon
-          ? <button onClick={() => changeIcon()} className='heart_button' aria-label="heart">
-              <img src={ Heart } className='heart_icon' alt='Heart Icon'/>
-            </button>
-          : <button onClick={() => changeIcon()} className='star_button' aria-label="star">
-              <img src={ Star } className='star_icon' alt='Star Icon'/>
-            </button>}
+        <section className='dropdown_section'>
+          <select onChange={(e) => { changeSKU(e.target.value) }} className='size_dropdown'>
+            <option hidden>SELECT SIZE</option>
+            {Object.values(currentStyle.skus).map(({ quantity, size }, index) => (
+              <option value={quantity} key={index}>
+                {size}
+              </option>
+            ))}
+          </select>
+          <select className='quantity_dropdown'>
+            <option hidden>_</option>
+            {quantityArray(quantity).map((inventory, index) =>
+              (!inventory
+                ? <option>OUT OF STOCK</option>
+                : <option key={index}>{inventory}</option>))}
+          </select>
+        </section>
+        <section className='add_to_cart_section'>
+          <button className='cart' aria-label="add to cart" onClick={() => addToCartClick()}>ADD TO CART</button>
+          {icon
+            ? <button onClick={() => changeIcon()} className='heart_button' aria-label="heart">
+                <img src={ Heart } className='heart_icon' alt='Heart Icon'/>
+              </button>
+            : <button onClick={() => changeIcon()} className='star_button' aria-label="star">
+                <img src={ Star } className='star_icon' alt='Star Icon'/>
+              </button>}
+        </section>
       </section>
   )
 }
