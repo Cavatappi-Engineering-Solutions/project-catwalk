5c7273a62f65cc39f79c2af4d81d04bd2cd1b431
timapar <timothy_parrish@hotmail.com>
2021-06-18 11:56:51 -0400

    switch empty object to undefined for initial state

--- client/src/stateManagement/reducers/__tests__/charcterReducer.test.js
+++ client/src/stateManagement/reducers/__tests__/charcterReducer.test.js
@@ -3,12 +3,12 @@ import characterReducer from '../characterReducer.js'
 
 describe('Each case should return a properly formatted object', () => {
   it('should return an empty object', () => {
-    const result = characterReducer({}, {})
+    const result = characterReducer(undefined, {})
     expect(result).toEqual({})
   })
 
   it('should return an object with characteristics and meanings', () => {
-    const result = characterReducer({}, { type: 'GET_CHARACTER', payload: { Quality: {} } })
+    const result = characterReducer(undefined, { type: 'GET_CHARACTER', payload: { Quality: {} } })
     expect(result).toEqual({ Quality: { low: 'Poor', high: 'Perfect' } })
   })
 })
