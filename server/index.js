d59f2dde1767d0608369cf668d81d0ebc01e1e80
timapar <timothy_parrish@hotmail.com>
2021-06-19 16:17:09 -0400

    update to forward all requests to api with authentication

--- server/index.js
+++ server/index.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const path = require('path')
+const config = require('../config.js')
+const axios = require('axios')
 
 const app = express()
 const PORT = process.env.PORT || 1337
 
 app.use(express.static(path.join(__dirname, '../client/public/')))
 
-app.get('*', (req, res) => {
+app.all('/api/*', (req, res) => {
+  const url = req.url
+  const method = req.method
+  const body = req.body
+  const reqConfig = {
+    method,
+    url: `https://app-hrsei-api.herokuapp.com${url}`,
+    headers: { Authorization: config.API_KEY },
+    body
+  }
+  axios(reqConfig)
+    .then((results) => res.send(results.data))
+    .catch((err) => {
+      console.log('API forward failed: ', err.response.status)
+      res.sendStatus(err.response.status)
+    })
+})
+
+app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'))
 })
 
