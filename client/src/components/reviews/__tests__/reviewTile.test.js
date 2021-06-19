09663ca54e545e776755d87b3d252db5519933a6
timapar <timothy_parrish@hotmail.com>
2021-06-19 17:08:58 -0400

    update test to handle change made to date

--- client/src/components/reviews/__tests__/reviewTile.test.js
+++ client/src/components/reviews/__tests__/reviewTile.test.js
@@ -4,16 +4,21 @@ import renderer from 'react-test-renderer'
 import Review from '../reviewTile.jsx'
 
 it('renders correctly', () => {
-  const tree = renderer.create(<Review
-    data={{
-      rating: 5,
-      summary: 'summary',
-      body: 'body',
-      recommend: true,
-      response: 'response',
-      helpfulness: 4,
-      review_id: 1234
-    }}
-  />).toJSON()
+  const tree = renderer
+    .create(
+      <Review
+        data={{
+          rating: 5,
+          summary: 'summary',
+          body: 'body',
+          recommend: true,
+          response: 'response',
+          helpfulness: 4,
+          review_id: 1234,
+          date: '2021-06-19T00:00:00.000Z'
+        }}
+      />
+    )
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
